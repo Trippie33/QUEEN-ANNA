a4193ccdd030db468d36c342d9aa8b8c15587bb0
Trippie33 <133229315+Trippie33@users.noreply.github.com>
2025-04-15 12:15:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,8 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //----------------------------------------------------------------------------
 
-global.mods = ['24105114159'] 
+global.mods = ['2349037546246
+               '] 
 global.prems = ['24105114159']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
